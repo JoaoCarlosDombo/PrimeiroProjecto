58d38b7b20ea72ad8c99a801049d456393ef756d
Joao Carlos <jhoncarlity2@gmail.com>
2022-10-18 09:25:13 +0100

    mostar na consola o post

--- react002/src/App.jsx
+++ react002/src/App.jsx
@@ -18,6 +18,7 @@ export default function App() {
   useEffect(() => {
     axios.get("https://primeiro-web-site.herokuapp.com/").then((response) => {
       setUsuarios(response.data);
+      console.log(response.data);
     });
   }, []);
 
