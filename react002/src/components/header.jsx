fabab354a03132adcf062892b4a765934644dc32
Joao Carlos <jhoncarlity2@gmail.com>
2022-10-18 11:23:41 +0100

    alteraçao da visualização da header

--- react002/src/components/header.jsx
+++ react002/src/components/header.jsx
@@ -1,10 +1,12 @@
 import React from "react";
-
+import "../components/header.css";
 export default function Header() {
   return (
     <>
-      <h1>Lista de Postes</h1>
-      <hr />
+      <div className="header">
+        <h1>Lista de Postes</h1>
+        <p>Minha primeira experiência com node.js e react - João Carlos </p>
+      </div>
     </>
   );
 }
